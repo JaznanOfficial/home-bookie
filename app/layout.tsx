738f238bea83e1d09f509fe754a6f03ed6b4199a
JaznanOfficial <jaznanofficial@gmail.com>
2023-04-17 10:31:04 +0600

    register modal done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar";
 import ClientOnly from "@/components/ClientOnly";
-import Modal from "@/components/modals/Modal";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 export const metadata = {
     title: "HomeBookie",
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <body className={font.className}>
                 <ClientOnly>
-                    <Modal actionLabel={"Submit"} title={"Hello world"} isOpen />
+                    <RegisterModal />
                     <Navbar />
                 </ClientOnly>
                 {children}
--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -39,7 +39,7 @@ const Modal: React.FC<ModalProps> = ({
             return;
         }
 
-        setShowModal(isOpen);
+        setShowModal(false);
         setTimeout(() => {
             onClose();
         }, 300);
