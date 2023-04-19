8d689f870ae9bc02c67fe406ad3ba3027ac831e9
JaznanOfficial <jaznanofficial@gmail.com>
2023-04-19 09:43:39 +0600

    some changes

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -33,7 +33,7 @@ const Input: React.FC<InputProps> = ({
                 type={type}
                 id={id}
                 disabled={disabled}
-                {...register(id, { required })}
+                {...register?.(id, { required })}
                 placeholder=""
                 className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed ${
                     formatPrice ? "pl-9" : "pl-4"
